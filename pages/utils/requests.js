eae52cd1797ecddcb5353a3524fbf5d3178e253d
alamgir8 <ahossain38900@gmail.com>
2021-06-26 07:14:24 +0600

    updated all

--- pages/utils/requests.js
+++ pages/utils/requests.js
@@ -1,8 +1,7 @@
-/* eslint-disable import/no-anonymous-default-export */
 const API_KEY=process.env.API_KEY;
 
 
-export default {
+export default movieData = {
   fetchTrending: {
     title: "Trending",
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
