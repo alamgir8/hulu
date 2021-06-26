68f65d1f1b608734aee287861309af9e8af0214c
alamgir8 <ahossain38900@gmail.com>
2021-06-26 07:22:47 +0600

    updated

--- pages/utils/requests.js
+++ pages/utils/requests.js
@@ -2,7 +2,7 @@
 const API_KEY=process.env.API_KEY;
 
 
-export default {
+const moveData = {
   fetchTrending: {
     title: "Trending",
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -48,3 +48,5 @@ export default {
     url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
   },
 };
+
+export default moveData;
